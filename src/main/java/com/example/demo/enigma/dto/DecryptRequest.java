240fe36325a6c0284862ceb37831fcaa67d4c2fa
Akhil <dddd>
2022-04-30 21:51:19 +0530

    Added decrypt method

---
+++ src/main/java/com/example/demo/enigma/dto/DecryptRequest.java
@@ -0,0 +1,14 @@
+package com.example.demo.enigma.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DecryptRequest extends Settings {
+    private String message;
+}
